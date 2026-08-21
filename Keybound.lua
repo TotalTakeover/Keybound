b828b497b9e3b0c4146d4bcaa80ccc4ce24251e8
Total <39064108+TotalTakeover@users.noreply.github.com>
2026-08-20 17:49:47 -0700

    VSCode LuaLS Documentation + More
    Holy shit I'm a real dev now!
    
    - Reworked the api to instead plug into the `Keybind` api, adding new methods, instead of wrapping around them.
      - This means you can plug a config directly into a keybind, not just along side it.
    - Added documentation to the library for better control.
      - Obv I will tweak this later on. Point is, this is far better than it was before
    - Renamed variables/symbols
    - Rewrote comments

--- Keybound.lua
+++ Keybound.lua
@@ -8,56 +8,68 @@
 --         \ \__\ \ \_______\   \ \__\ \ \__\ \__\ \_______\
 --          \|__|  \|_______|    \|__|  \|__|\|__|\|_______|
 --
--- Version: 1.0.2
+-- Version: 1.1.0
 
 -- Host only instructions
 if not host:isHost() then return end
 
--- Create API
-local keyAPI = {}
+-- Keybind API's metatable index.
+local keybindIndex = figuraMetatables.Keybind.__index
 
--- Keys table
+-- Functions that are created/overwritten for the keybind API to do custom things.
+---@class Keybind
+local newMethods = {}
+
+-- A table that holds keybinds for later updating.
 local keys = {}
 
--- Store a new keybind
-function keyAPI.new(keybind, cfgName)
+-- Attaches a config key to a keybind, and attempts to set its key to the config's value.
+---@param cfgName string #
+-- The config's key that will be added to the keybind.
+function newMethods:setConfig(cfgName)
 	
 	-- Attach bind
-	keybind:key(config:load(cfgName) or keybind:getKey())
+	self:key(config:load(cfgName) or self:getKey())
 	
 	-- Store keybind
-	keys[keybind] = {
-		key = keybind:getKey(),
+	keys[self] = {
+		key = self:getKey(),
 		cfg = cfgName
 	}
 	
 	-- Return keybind
-	return keybind
+	return self
 	
 end
 
--- Sync on tick
+-- Copy to alias
+newMethods.config = newMethods.setConfig
+
+-- Apply new functions to API
+function figuraMetatables.Keybind.__index(self, key)
+	return newMethods[key] or keybindIndex(self, key)
+end
+
+-- This tick function checks if the user is on the Figura Keybind screen.  
+-- If they are, it will compare the keybinds last state, and if they do not match, stores the new key to the config.
 events.TICK:register(function()
 	if host:getScreen() == "org.figuramc.figura.gui.screens.KeybindScreen" then
-		for k, v in pairs(keys) do
+		for keybind, keyTbl in pairs(keys) do
 			
 			-- Get current key
-			local key = k:getKey()
+			local currKey = keybind:getKey()
 			
 			-- Compare keys
-			if v.key ~= key then
+			if keyTbl.key ~= currKey then
 				
-				-- Save to config
-				config:save(v.cfg, key)
+				-- Save new key to config
+				config:save(keyTbl.cfg, currKey)
 				
-				-- Store data
-				v.key = key
+				-- Store new key
+				keyTbl.key = currKey
 				
 			end
 			
 		end
 	end
 end, "tickKeys")
-
--- Return API
-return keyAPI
